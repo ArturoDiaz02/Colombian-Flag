3c6c91ae4b19da2dc7ad9f93c6d4c8ae8316588b
Carlos Arturo Diaz Artiaga <diazartiagacarlosarturo@gmail.com>
2021-05-21 15:56:28 -0500

    Final

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -8,11 +8,14 @@ public class Main {
     public static final String YELLOW_BACKGROUND = "\u001B[43m";
     public static final String BLUE_BACKGROUND = "\u001B[44m";
     public static final String RED_BACKGROUND = "\u001B[41m";
+    private static ClassThread thread1;
+    private static ClassThread thread2;
+    private static ClassThread thread3;
 
     public static void main(String[] args){
-        ClassThread thread1 = new ClassThread(new Cursor(14, 0, YELLOW_BACKGROUND, 10));
-        ClassThread thread2 = new ClassThread(new Cursor(7, 14, BLUE_BACKGROUND, 26));
-        ClassThread thread3 = new ClassThread(new Cursor(7, 21, RED_BACKGROUND, 33));
+        thread1 = new ClassThread(new Cursor(14, 0, YELLOW_BACKGROUND, 10));
+        thread2 = new ClassThread(new Cursor(7, 14, BLUE_BACKGROUND, 26));
+        thread3 = new ClassThread(new Cursor(7, 21, RED_BACKGROUND, 33));
 
         System.out.print("\u001b[2J");//Clear screen
 
